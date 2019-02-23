88f236a8c97e9876fc86d457a261d1a27f6ebe86
Ari Maulana <ri.ganjil@gmail.com>
2019-02-23 12:43:02 +0700

    initial commit

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+use Illuminate\Support\Facades\Hash;
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name'     => $faker->name,
+        'email'    => $faker->unique()->email,
+        'password' => Hash::make('12345'),
+    ];
+});
